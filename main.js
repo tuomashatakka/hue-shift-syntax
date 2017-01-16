1f703909d49bfbe8d84fe2ece11930544a501b8e
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-01-16 20:24:04 +0200

    Configuration options & a loadful of all things bright & beautiful

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 'use babel'
+import applyStyles from './lib/config'
 
+
+let disposables
 export default {
-  activate: () => {
-    atom.notifications.addInfo("Hue Shift syntax theme activated");
-  },
-  deactivate: () => {
-    atom.notifications.addInfo("Hue Shift syntax theme deactivated")
-  }
+
+    activate: () => {
+      atom.notifications.addInfo("Hue Shift syntax theme activated");
+      disposables = atom.config.observe(
+        'hue-shift-syntax',
+        () => {
+          let conf = atom.config.get('hue-shift-syntax')
+          console.log(conf);
+          applyStyles({
+            ...conf.main,
+            ...conf.filter,
+            ...conf.modifiers,
+          })
+        })
+    },
+
+    deactivate: () => {
+      atom.notifications.addInfo("Hue Shift syntax theme deactivated")
+      disposables.dispose()
+    }
 }
